6247103c24b81084934b3be12266d31f78807a28
Rocio Ayelén Garcia <rocioayelengarcia177@gmail.com>
2024-11-08 13:06:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,118 +14,101 @@ hoverVideo.addEventListener("mouseout", function() {
     staticImage.style.display = "block"; 
 });
 
-// nav mobile
-function nav() {
-  document.getElementById("navabierto").classList.toggle("mostrarnav");
+//galeria
+function dropdown() {
+  document.getElementById("cincuentas").classList.toggle("show");
 }
 window.onclick = function(event) {
-  if (!event.target.matches('.botonnav')) {
-    var dropdowns = document.getElementsByClassName("listanav");
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content, flechita-galeria");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('mostrarnav')) {
-        openDropdown.classList.remove('mostrarnav');
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
 }
-
-  //galeria
-  function dropdown() {
-    document.getElementById("cincuentas").classList.toggle("show");
-  }
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content, flechita-galeria");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  
+function dropdown2() {
+  document.getElementById("sesentas").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content, flechita-galeria");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
+}
   
-  function dropdown2() {
-    document.getElementById("sesentas").classList.toggle("show");
-  }
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content, flechita-galeria");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+function dropdown3() {
+  document.getElementById("setentas").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content, flechita-galeria");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
+}
   
-  function dropdown3() {
-    document.getElementById("setentas").classList.toggle("show");
-  }
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content, flechita-galeria");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+function dropdown4() {
+  document.getElementById("ochentas").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content, flechita-galeria");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
+}
+    
+var element = document.querySelector(".flechita-galeria");
   
-  function dropdown4() {
-    document.getElementById("ochentas").classList.toggle("show");
-  }
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content, flechita-galeria");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
-         
-  var element = document.querySelector(".flechita-galeria");
-  
-  element.addEventListener("click", function () {  
-    element.classList.toggle("back");
-  });
+element.addEventListener("click", function () {  
+  element.classList.toggle("back");
+});
 
 // mapas pages
 var myImage=document.querySelector("#img-id");
 
 function linea33()
 {
-  myImage.src="img/mapa-33.png"
+  myImage.src="img/mapa-33.png";
 }
 
 function linea37()
 {
-  myImage.src="img/mapa-37.png"
+  myImage.src="img/mapa-37.png";
 }
 
 function linea60()
 {
-  myImage.src="img/mapa-60.png"
+  myImage.src="img/mapa-60.png";
 }
 
 function linea79()
 {
-  myImage.src="img/mapa-79.png"
+  myImage.src="img/mapa-79.png";
 }
 
 function linea160()
 {
-  myImage.src="img/mapa-160.png"
+  myImage.src="img/mapa-160.png";
 }
